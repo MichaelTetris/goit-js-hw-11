f7449167128e651037054236d2c1f26735663f7e
MichaelTetris <125746441+MichaelTetris@users.noreply.github.com>
2024-04-17 11:20:15 +0300

    styles css

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,20 @@
-export function searchImages(searchWord, myGallery){
+export function searchImages(stringSearch, myGallery) {
+  const API_KEY = '43418044-448fc0127227a847b3808d395';
+  const url = 'https://pixabay.com/api/';
 
-};
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: stringSearch,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 9,
+  });
+
+  return fetch(`${url}?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
